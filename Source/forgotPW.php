a99768f424e2bdcbbfcec31c5ea7d25528f6bf30
vuthihonggam <you@example.com>
2022-01-10 13:15:40 +0700

    fix frontend forgotPW

--- Source/forgotPW.php
+++ Source/forgotPW.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Nhập mật khẩu</title>
+  <title>Quên mật khẩu</title>
   <meta charset="utf-8">
-  <link rel="stylesheet" href="css/SignInPassWord.css">
+  <link rel="stylesheet" href="css/forget.css">
   <!-- CSS only -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <!-- JavaScript Bundle with Popper -->
